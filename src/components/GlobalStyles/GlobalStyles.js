84a8d9cf511a3abd321a64faf23a7438f5349be8
Andres Ferrada <an.ferrada@gmail.com>
2026-01-31 23:57:53 -0300

    use fragmented variable in gray-700 and color backdrop

--- src/components/GlobalStyles/GlobalStyles.js
+++ src/components/GlobalStyles/GlobalStyles.js
@@ -65,17 +65,18 @@ table {
   */
   isolation: isolate;
 
+--gray-700-values: 220 5% 40%;
 --white: hsl(0deg 0% 100%);
 --gray-100: hsl(185deg 5% 95%);
 --gray-300: hsl(190deg 5% 80%);
 --gray-500: hsl(196deg 4% 60%);
---gray-700: hsl(220deg 5% 40%);
+--gray-700: hsl(var(--gray-700-values));
 --gray-900: hsl(220deg 3% 20%);
 --red: hsl(0deg 65% 47%);
 --green: hsl(120deg 65% 47%);
 --primary: hsl(340deg 65% 47%);
 --secondary: hsl(240deg 60% 63%);
---color-backdrop: hsla(220, 5%, 40%, 0.8);
+--color-backdrop: hsl(var(--gray-700-values) / 0.8);
 
 
 
