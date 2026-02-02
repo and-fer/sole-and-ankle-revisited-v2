76d2d5a16bb8588296b023b91e0a485b6d4b5a47
Andres Ferrada <an.ferrada@gmail.com>
2026-02-02 17:29:48 -0300

    refactor padding between main header and body

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -22,6 +22,10 @@ const App = () => {
 const Main = styled.main`
   padding: 64px 32px;
 
+  @media ${QUERIES.tabletAndSmaller} {
+    padding: 48px 32px;
+  }
+
   @media ${QUERIES.phoneAndSmaller} {
     padding-left: 16px;
     padding-right: 16px;
